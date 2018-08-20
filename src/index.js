088f62adc840a63b22fce69f06ce1ec26ef0ab85
Rushing <liming20110711@163.com>
2018-08-21 07:45:04 +0800

    update:20180821-02

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // App组件，大写字母开头
 import TodoList from './TodoList';
-// 1
 import './style.css'
 
 ReactDOM.render(<TodoList/>, document.getElementById('root'));
