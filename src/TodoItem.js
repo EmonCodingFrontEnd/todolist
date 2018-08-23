2cc9a4b98abd0f8846bc36c4faedbfb728771aec
Rushing <liming20110711@163.com>
2018-08-23 08:29:12 +0800

    update:20180823-01

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -8,25 +8,47 @@ class TodoItem extends Component {
         this.handleDelete = this.handleDelete.bind(this);
     }
 
-    // 子组件如何想和父组件通信，子组件要调用父组件传递过来的方法
+    // 子组件如何向父组件通信，子组件要调用父组件传递过来的方法
 
     handleDelete() {
         const {deleteItem, index} = this.props;
         deleteItem(index);
     }
 
+    /**
+     * 1.一个组件要从父组件接收参数
+     * 2.只要父组件的render函数被重新执行，子组件的这个生命周期函数就会被执行
+     * 3.如果这组件第一次存在于父组件中，不会执行
+     * 4.如果这个组件之前已经存在于父组件中，才会执行
+     */
+    componentWillReceiveProps() {
+        console.log('child componentWillReceiveProps');
+    }
+
+    // 当组件即将被从页面中剔除时执行
+    componentWillUnmount() {
+        console.log('child componentWillUnmount');
+    }
+
     render() {
-        const {content} = this.props;
+        const {content, test} = this.props;
+        // JSX -> createElement -> 虚拟DOM（JS对象） -> 真实的DOM对象
+        // return React.createElement('div', {}, 'item');
         return (
-            <div onClick={this.handleDelete} dangerouslySetInnerHTML={{__html: content}}/>
+            <div onClick={this.handleDelete} dangerouslySetInnerHTML={{__html: test + '-' + content}}/>
         )
     }
 }
 
 TodoItem.propTypes = {
-    content: PropTypes.string,
+    test: PropTypes.string.isRequired,
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     deleteItem: PropTypes.func,
     index: PropTypes.number
+};
+
+TodoItem.defaultProps = {
+    test: 'hello world'
 };
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import TodoItem from './TodoItem';
+import Test from './Test';
 import './style.css'
 
 // 定义一个React组件
@@ -8,6 +9,7 @@ class TodoList extends Component {
 
     constructor(props) {
         super(props);
+        // 当组件的state或者props发生变化时，render函数就会执行
         this.state = {
             list: [],
             inputValue: ''
@@ -29,7 +31,9 @@ class TodoList extends Component {
         this.setState((prevState) => ({
             list: [...prevState.list, prevState.inputValue],
             inputValue: ''
-        }));
+        }), () => {
+            console.log(this.ul.querySelectorAll('div').length);
+        });
     }
 
     // 父组件通过属性的形式向子组件传递参数，子组件通过props接受父组件传递的参数
@@ -55,7 +59,13 @@ class TodoList extends Component {
         )
     }
 
+    // 在组件即将被挂载到页面的时刻自动执行
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
     render() {
+        console.log('render');
         return (
             <Fragment>
                 <div>
@@ -67,13 +77,41 @@ class TodoList extends Component {
                     <input id="insertArea"
                            className={'input'}
                            value={this.state.inputValue}
-                           onChange={this.handleInputChange}/>
+                           onChange={this.handleInputChange}
+                    />
                     <button className={'red-btn'} onClick={this.handleBtnClick}>add</button>
                 </div>
-                <ul>{this.getTodoItems()}</ul>
+                <ul ref={(ul) => {
+                    this.ul = ul
+                }}>{this.getTodoItems()}</ul>
+                <Test content={this.state.inputValue}/>
             </Fragment>
         );
     }
+
+    // 组件被挂载到页面之后自动执行
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    // 在组件被更新之前，会自动被执行
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate');
+        // true:需要执行;false:不需要执行
+        return true;
+    }
+
+    // 组件被更新之前，它会自动执行；但会在shouldComponentUpdate返回true情况下
+    componentWillUpdate() {
+        console.log('componentWillUpdate');
+    }
+
+    // 在组件被更新之后，会被执行
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+
+
 }
 
 export default TodoList;
